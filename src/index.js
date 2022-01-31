899a64288380696c9d96223af57a0e006eb1cfbc
Taras <taraskin2012@gmail.com>
2022-01-31 20:48:00 +0200

    add absolute import

--- src/index.js
+++ src/index.js
@@ -1,39 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const Header = (props) => {
-   return <h1>
-     {props.id} Hello {props.title}
-   </h1>
-}
-
-const Text = () => {
-    return (
-        <>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ab
-                aspernatur earum aliquam commodi, maiores doloribus. Officia
-                iste minima facilis labore dolore error odit impedit. Iusto
-                veniam molestiae corporis animi!
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ab
-                aspernatur earum aliquam commodi, maiores doloribus. Officia
-                iste minima facilis labore dolore error odit impedit. Iusto
-                veniam molestiae corporis animi!
-            </p>
-        </>
-    )
-}
-
-const App = () => {
-    return (
-        <>
-            <Header title="React" id={1} />
-          <Header title="App.js" id={2} />
-            <Text />
-        </>
-    )
-}
+import App from "components/App"
 
 ReactDom.render(<App />, document.getElementById('root'))
