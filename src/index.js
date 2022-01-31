69832609fa9b0e7bb33bd0b37971ab4cd85d4bf3
Taras <taraskin2012@gmail.com>
2022-01-31 20:21:21 +0200

    add react fragment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const el = <h1>Hello React.js</h1>
+const Header = () => {
+    return <h1>Hello App component</h1>
+}
 
-ReactDom.render(el, document.getElementById('root'))
+const Text = () => {
+    return (
+        <>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ab
+                aspernatur earum aliquam commodi, maiores doloribus. Officia
+                iste minima facilis labore dolore error odit impedit. Iusto
+                veniam molestiae corporis animi!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ab
+                aspernatur earum aliquam commodi, maiores doloribus. Officia
+                iste minima facilis labore dolore error odit impedit. Iusto
+                veniam molestiae corporis animi!
+            </p>
+        </>
+    )
+}
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Text />
+        </>
+    )
+}
+
+ReactDom.render(<App />, document.getElementById('root'))
