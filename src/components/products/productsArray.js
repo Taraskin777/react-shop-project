c4efb9e7d2a564f1669063fc482c6e5bc88b37cb
Taras <taraskin2012@gmail.com>
2022-02-09 19:29:11 +0200

    add destructurisation

--- src/components/products/productsArray.js
+++ src/components/products/productsArray.js
@@ -1,40 +1,46 @@
 export const productsArray = [
-   {
+    {
+       id:1,
        name: 'iPhone 13 Pro',
        description: 'This is iPhone 13 Pro',
        capacity: '128',
        type: 'phone',
        price: '1100',
    },
-   {
+    {
+       id:2,
        name: 'iPhone 12 Pro',
        description: 'This is iPhone 12 Pro',
        capacity: '128',
        type: 'phone',
        price: '2000',
    },
-   {
+    {
+       id:3,
        name: 'iPhone 13 Pro',
        description: 'This is iPhone 13',
        capacity: '128',
        type: 'phone',
        price: '1500',
    },
-   {
+    {
+       id:4,
        name: 'iPhone 10 Pro',
        description: 'This is iPhone 10 Pro',
        capacity: '128',
        type: 'phone',
        price: '1300',
    },
-   {
+    {
+       id:5,
        name: 'iPhone 11 Pro',
        description: 'This is iPhone 11 Pro',
        capacity: '128',
        type: 'phone',
        price: '2000',
    },
-   {
+    {
+       id:6,
        name: 'iPhone 11 Pro',
        description: 'This is iPhone 11 Pro',
        capacity: '128',
