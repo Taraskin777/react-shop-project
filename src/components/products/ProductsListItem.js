b14c5a9b21f0da14385c14b9ee66a7ad668e4910
Taras <taraskin2012@gmail.com>
2022-02-09 20:36:14 +0200

    add proptypes

--- src/components/products/ProductsListItem.js
+++ src/components/products/ProductsListItem.js
@@ -1,6 +1,7 @@
 import { Button, Card, CardActions, CardContent } from '@mui/material'
 import React from 'react'
 import './ProductsListItem.css'
+import PropTypes from 'prop-types'
 
 export const ProductsListItem = ({
     name,
@@ -29,4 +30,17 @@ export const ProductsListItem = ({
             </Card>
         </>
     )
+}
+
+ProductsListItem.propTypes = {
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    type: PropTypes.string.isRequired,
+    capacity: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string,
+}
+
+ProductsListItem.defaultProps = {
+    description: "No description..."
 }
