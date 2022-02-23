f626cbe6c0b40f5e74f826f6197668e6b0b94f5e
Taras <taraskin2012@gmail.com>
2022-02-23 20:19:04 +0200

    add name of products

--- src/components/Cart/CartHeader.js
+++ src/components/Cart/CartHeader.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import { keys } from 'lodash'
+import { productsArray } from 'components/products/productsArray'
 
 export const CartHeader = ({ productsInCart }) => {
     return (
         <div>
             {keys(productsInCart).map((productId) => (
-                <div key={productId}>
-                    {productId} : {productsInCart[productId]}
+                <div key={(productId)}>
+                    {productsArray[productId-1].name} : {productsInCart[productId]}
+                    
+                  
                 </div>
             ))}
         </div>
