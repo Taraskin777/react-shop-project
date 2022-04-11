b5e586eac71614cd7ab8887c6c99a89e9a561f0d
Taras Noga <taraskin2012@gmail.com>
2022-04-11 19:28:51 +0300

    add removeProductFromCart

--- src/components/Cart/CartProductListemItemExtended.js
+++ src/components/Cart/CartProductListemItemExtended.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Card, CardContent, Grid } from '@mui/material'
+import { Button, Card, CardContent, Grid } from '@mui/material'
 import { makeStyles } from '@mui/styles'
+import { Delete } from '@mui/icons-material'
 
 const useStyles = makeStyles({
     media: {
-        width: 100,
+        width: 150,
         height: 'auto',
     },
     cardWrap: {
@@ -25,6 +26,9 @@ export const CartProductListItemExtended = ({ product, productCount }) => {
                     <div>{product.name}</div>
                     <div>Price for one items: {product.price}</div>
                     <div>Count: {productCount}</div>
+                    <Button variant="outlined">
+                        <Delete />
+                    </Button>
                 </CardContent>
             </Card>
         </Grid>
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,24 +4,34 @@ import { Header } from 'containers/Header/Header'
 import { Main } from 'containers/Main/Main'
 import { Footer } from 'containers/Footer/Footer'
 import { productsArray } from 'components/products/productsArray'
+import _ from 'lodash'
 
 export const App = () => {
-    const [productsInCart, setproductsInCart] = useState({
+    const [productsInCart, setProductsInCart] = useState({
         1: 2,
         2: 2,
     })
 
     const addProductToCart = (id, count) => {
-        setproductsInCart((prevState) => ({
+        setProductsInCart((prevState) => ({
             ...prevState,
             [id]: (prevState[id] || 0) + count,
         }))
+    }
+
+    const removeProductFromCart = (productId) => {
+        setProductsInCart((prevState) => {
+            const prevProductsInCart = { ...prevState }
+            delete prevProductsInCart[productId]
+            return prevProductsInCart
+        })
     }
 
     return (
         <>
             <CssBaseline />
             <Header productsInCart={productsInCart} />
+            <button onClick={() => removeProductFromCart(1)}>Delete</button>
             <Main
                 addProductToCart={addProductToCart}
                 productsInCart={productsInCart}
