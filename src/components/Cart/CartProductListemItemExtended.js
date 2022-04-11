54bba2d36cf7aa58fec30990a7657f09a1643991
Taras Noga <taraskin2012@gmail.com>
2022-04-11 18:50:54 +0300

    add styles for CartProductListItemExtende

--- src/components/Cart/CartProductListemItemExtended.js
+++ src/components/Cart/CartProductListemItemExtended.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Card, CardContent, Grid } from '@mui/material'
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles({
+    media: {
+        width: 100,
+        height: 'auto',
+    },
+    cardWrap: {
+        display: 'flex',
+        padding: 15,
+    },
+})
 
 export const CartProductListItemExtended = ({ product, productCount }) => {
+    const classes = useStyles()
     return (
-        <div>
-            {product.name} :{productCount} : Extended
-        </div>
+        <Grid item xs={12} sm={4}>
+            <Card className={classes.cardWrap}>
+                <div>
+                    <img src={product.image} className={classes.media}></img>
+                </div>
+                <CardContent>
+                    <div>{product.name}</div>
+                    <div>Price for one items: {product.price}</div>
+                    <div>Count: {productCount}</div>
+                </CardContent>
+            </Card>
+        </Grid>
     )
 }
--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -2,18 +2,19 @@ import React from 'react'
 import { CartTotal } from 'components/Cart/CartTotal'
 import { CartProductList } from 'components/Cart/CartProductList'
 import { CartProductListItemExtended } from 'components/Cart/CartProductListemItemExtended'
+import { Grid } from '@mui/material'
 
 export const CartPage = ({ productsInCart }) => {
     return (
         <>
             <h1>Cart</h1>
-            <div>
+            <Grid container spacing={3}>
                 <CartProductList
                     productsInCart={productsInCart}
                     CartItem={CartProductListItemExtended}
                 />
-                <CartTotal productsInCart={productsInCart} />
-            </div>
+            </Grid>
+            <CartTotal productsInCart={productsInCart} />
         </>
     )
 }
